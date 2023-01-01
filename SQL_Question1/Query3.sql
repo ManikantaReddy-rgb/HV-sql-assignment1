b44625985e26b65df1f237c66630f2b90a9738fa
ManikantaReddy-rgb <84713050+ManikantaReddy-rgb@users.noreply.github.com>
2023-01-01 15:15:41 +0530

    Add files via upload

---
+++ SQL_Question1/Query3.sql
@@ -0,0 +1,11 @@
+INSERT INTO Employee.Employee_Details (Emp_id, Emp_name, Job_name, Manager_id, Hire_date, Salary, Dep_id)
+VALUES (00001, 'Akhil', 'PRESIDENT', 121131, '2010-10-22', 80000, 101),
+       (00002, 'ManikantaReddy', 'VICE PRESIDENT', 121132, '2010-12-28', 70000, 102),
+       (00003, 'Nikhil', 'MANAGER', 121133, '2011-02-09', 55000, 1003),
+       (00004, 'Varma', 'ASSISTANT MANAGER', 121134, '2011-07-19', 40000, 104),
+       (00005, 'Sai', 'ASSISTANT MANAGER', 121135, '2011-11-29', 40000, 104),
+       (00006, 'Teja', 'TEAM LEADER', 121136, '2013-11-30', 35000, 105),
+       (00007, 'revi', 'TEAM LEADER', 121137, '2014-02-14', 35000, 105),
+       (00008, 'prasad', 'DEVELOPER', 121138, '2014-06-24', 30000, 106),
+       (00009, 'pawan', 'DEVELOPER', 121139, '2014-08-14', 30000, 106),
+       (00010, 'Krishna', 'TRAINEE', 121140, '2015-04-01', 20000, 106);
